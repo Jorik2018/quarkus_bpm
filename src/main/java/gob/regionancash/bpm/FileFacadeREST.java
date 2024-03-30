401ef38dc968e335eff1b9edf1fb653b9ee61431
Erik Antonio Alarcon Pinedo <ealarcop@TRJ-M76GC7X5WV.local>
2024-03-30 12:34:56 -0500

    x

--- src/main/java/gob/regionancash/bpm/FileFacadeREST.java
+++ src/main/java/gob/regionancash/bpm/FileFacadeREST.java
@@ -37,7 +37,7 @@ public class FileFacadeREST {
         return Map.of("data", list);
     }
 
-    @GET
+    @POST
     @Path("/download")
     @Produces(MediaType.APPLICATION_OCTET_STREAM)
     public Response downloadFile(@QueryParam("filename") String filename) {
